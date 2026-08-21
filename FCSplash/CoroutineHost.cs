cccff38a2468eab2a80280635496cb6a87eee0ee
unknownjwly <120588365+unknownjwly@users.noreply.github.com>
2026-08-21 18:31:57 +0100

    Delay song finish

---
+++ FCSplash/CoroutineHost.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace FCSplash
+{
+    public class CoroutineHost : MonoBehaviour
+    {
+        private static CoroutineHost? _instance;
+
+        public static void Start(IEnumerator routine)
+        {
+            if (_instance == null)
+            {
+                GameObject obj = new GameObject("FCSplashCoroutineHost");
+                DontDestroyOnLoad(obj);
+                _instance = obj.AddComponent<CoroutineHost>();
+            }
+
+            _instance.StartCoroutine(_instance.RunRoutine(routine));
+        }
+
+        private IEnumerator RunRoutine(IEnumerator routine)
+        {
+            yield return StartCoroutine(routine);
+
+            if (transform.childCount == 0)
+            {
+                Destroy(gameObject);
+                _instance = null;
+            }
+        }
+    }
+}
---
+++ FCSplash/LevelFinishPatch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FCSplash
+{
+    [HarmonyPatch(typeof(StandardLevelScenesTransitionSetupDataSO), nameof(StandardLevelScenesTransitionSetupDataSO.Finish))]
+    public class LevelFinishPatch
+    {
+        private static bool _isWaiting = false;
+
+        public static bool Prefix(StandardLevelScenesTransitionSetupDataSO __instance, LevelCompletionResults levelCompletionResults)
+        {
+            if (_isWaiting) return true;
+
+            Plugin.Log.Info($"LevelFinishPatch: Level ended with state -> {levelCompletionResults.levelEndStateType}");
+
+            if (levelCompletionResults.levelEndStateType == LevelCompletionResults.LevelEndStateType.Cleared)
+            {
+                Plugin.Log.Info("LevelFinishPatch: Level Cleared. Waiting 1 second for FC animation...");
+                CoroutineHost.Start(DelayedFinishRoutine(__instance, levelCompletionResults));
+                return false; 
+            }
+
+            return true;
+        }
+        
+        private static IEnumerator DelayedFinishRoutine(StandardLevelScenesTransitionSetupDataSO instance, LevelCompletionResults results)
+        {
+            yield return new WaitForSeconds(1.25f);
+            
+            _isWaiting = true;
+            instance.Finish(results);
+            _isWaiting = false;
+        }
+    }
+}
