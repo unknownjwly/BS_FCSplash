f323e243e442de602dd6399d9c58a71edda3235b
unknownjwly <120588365+unknownjwly@users.noreply.github.com>
2026-08-21 18:38:01 +0100

    Delay song finish

--- FCSplash/LevelFinishPatch.cs
+++ FCSplash/LevelFinishPatch.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using HarmonyLib;
 using UnityEngine;
 
@@ -13,11 +13,8 @@ namespace FCSplash
         {
             if (_isWaiting) return true;
 
-            Plugin.Log.Info($"LevelFinishPatch: Level ended with state -> {levelCompletionResults.levelEndStateType}");
-
             if (levelCompletionResults.levelEndStateType == LevelCompletionResults.LevelEndStateType.Cleared)
             {
-                Plugin.Log.Info("LevelFinishPatch: Level Cleared. Waiting 1 second for FC animation...");
                 CoroutineHost.Start(DelayedFinishRoutine(__instance, levelCompletionResults));
                 return false; 
             }
